0cd6e996c735832eb67f8e24bfb93bdca9bd8c4d
Julian Eduardo Guarnizo Parra <julii2002@hotmail.com>
2025-12-30 12:39:17 -0500

    initial commit

---
+++ src/main/java/com/julianguarnizo/gestioncursos/GestioncursosApplication.java
@@ -0,0 +1,13 @@
+package com.julianguarnizo.gestioncursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioncursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julianguarnizo/gestioncursos/GestioncursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianguarnizo.gestioncursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
